976cdfe3abfbbb80ac8b4c373dd394738f049a12
Patrik Hildenborg Pc <phildenborg@gmail.com>
2025-05-17 00:25:55 +0200

    .-

---
+++ SarasBlogg/Extensions/RegexExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SarasBlogg.Extensions
+{
+    public static class RegexExtensions
+    {
+        //public static bool IsMatchRegex(this string input, string pattern)
+        //{
+        //    if (input == null || pattern == null)
+        //        return false;
+
+        //    return Regex.IsMatch(input, pattern);
+        //}
+    }
+}
