afb239acbea6a92c8f30c7748325a6592ffed26d
Patrik Hildenborg Pc <phildenborg@gmail.com>
2025-06-16 15:19:11 +0200

    more design

---
+++ SarasBlogg/Pages/Blogg.cshtml.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SarasBlogg.Services;
+using SarasBlogg.ViewModels;
+
+namespace SarasBlogg.Pages
+{
+    public class BloggModel : PageModel
+    {
+        private readonly BloggService _bloggService;
+
+        public BloggModel(BloggService bloggService)
+        {
+            _bloggService = bloggService;
+        }
+
+        [BindProperty]
+        public BloggViewModel ViewModel { get; set; } = new();
+
+        public async Task OnGetAsync(int showId, int id)
+        {
+            if (showId != 0)
+            {
+                await _bloggService.UpdateViewCountAsync(showId);
+            }
+            ViewModel = await _bloggService.GetBloggViewModelAsync(false, showId);
+        }
+
+        public async Task<IActionResult> OnPostAsync(int deleteCommentId)
+        {
+            if (deleteCommentId != 0)
+            {
+                var comment = await _bloggService.GetCommentAsync(deleteCommentId);
+                if (comment != null)
+                {
+                    await _bloggService.DeleteCommentAsync(deleteCommentId);
+                    return RedirectToPage("./Index", new { showId = comment.BloggId });
+                }
+            }
+
+            if (ModelState.IsValid && ViewModel.Comment?.Id == null)
+            {
+                string errorMessage = await _bloggService.SaveCommentAsync(ViewModel.Comment);
+
+                if (!string.IsNullOrWhiteSpace(errorMessage))
+                {
+                    ModelState.AddModelError("Comment.Content", errorMessage);
+                    //ModelState.AddModelError("Comment.Name", errorMessage);
+
+                    ViewModel = await _bloggService.GetBloggViewModelAsync(false, ViewModel.Comment?.BloggId ?? 0);
+
+                    return Page();
+                }
+            }
+
+            return RedirectToPage("./Index", new { showId = ViewModel.Comment?.BloggId, commentId = ViewModel.Comment?.BloggId });
+        }
+    }
+}
--- SarasBlogg/Pages/Index.cshtml.cs
+++ SarasBlogg/Pages/Index.cshtml.cs
@@ -1,8 +1,8 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using SarasBlogg.ViewModels;
 using SarasBlogg.Services;
 using SarasBlogg.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SarasBlogg.Pages
 {
@@ -10,51 +10,20 @@ namespace SarasBlogg.Pages
     {
         private readonly BloggService _bloggService;
 
+        public IEnumerable<Blogg> LatestPosts { get; set; }
+
         public IndexModel(BloggService bloggService)
         {
             _bloggService = bloggService;
         }
 
-        [BindProperty]
-        public BloggViewModel ViewModel { get; set; } = new();
-
-        public async Task OnGetAsync(int showId, int id)
+        public void OnGet()
         {
-            if (showId != 0)
-            {
-                await _bloggService.UpdateViewCountAsync(showId);
-            }
-            ViewModel = await _bloggService.GetBloggViewModelAsync(false, showId);
-        }
-
-        public async Task<IActionResult> OnPostAsync(int deleteCommentId)
-        {
-            if (deleteCommentId != 0)
-            {
-                var comment = await _bloggService.GetCommentAsync(deleteCommentId);
-                if (comment != null)
-                {
-                    await _bloggService.DeleteCommentAsync(deleteCommentId);
-                    return RedirectToPage("./Index", new { showId = comment.BloggId });
-                }
-            }
-
-            if (ModelState.IsValid && ViewModel.Comment?.Id == null)
-            {
-                string errorMessage = await _bloggService.SaveCommentAsync(ViewModel.Comment);
-
-                if (!string.IsNullOrWhiteSpace(errorMessage))
-                {
-                    ModelState.AddModelError("Comment.Content", errorMessage);
-                    //ModelState.AddModelError("Comment.Name", errorMessage);
-
-                    ViewModel = await _bloggService.GetBloggViewModelAsync(false, ViewModel.Comment?.BloggId ?? 0);
-
-                    return Page();
-                }
-            }
-
-            return RedirectToPage("./Index", new { showId = ViewModel.Comment?.BloggId, commentId = ViewModel.Comment?.BloggId });
+            LatestPosts = _bloggService
+                .GetAllBloggs()
+                .OrderByDescending(p => p.LaunchDate)
+                .Take(2)
+                .ToList();
         }
     }
 }
