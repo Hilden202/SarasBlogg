22d16eeb223e0662beb55c099ef927a23d3a048a
Patrik Hildenborg Pc <phildenborg@gmail.com>
2025-08-17 04:23:55 +0200

    comfirm site fixed

--- SarasBlogg/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ SarasBlogg/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -1,6 +1,4 @@
-﻿// Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SarasBlogg.Areas.Identity.Pages.Account
@@ -8,28 +6,8 @@ namespace SarasBlogg.Areas.Identity.Pages.Account
     [AllowAnonymous]
     public class RegisterConfirmationModel : PageModel
     {
-        public string Email { get; set; } = "";
-        public bool DisplayConfirmAccountLink { get; set; }
-        public string? EmailConfirmationUrl { get; set; }
-
-        public IActionResult OnGet(string email, string? confirmUrl = null)
+        public void OnGet()
         {
-            if (string.IsNullOrWhiteSpace(email))
-                return RedirectToPage("/Index");
-
-            Email = email;
-
-            if (!string.IsNullOrWhiteSpace(confirmUrl))
-            {
-                DisplayConfirmAccountLink = true;
-                EmailConfirmationUrl = confirmUrl;
-            }
-            else
-            {
-                DisplayConfirmAccountLink = false;
-            }
-
-            return Page();
         }
     }
 }
